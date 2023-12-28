9f96fc2e645b6aa0a1d493251860866712483008
Andyauk <47583709+Andyauk@users.noreply.github.com>
2023-12-28 11:10:58 +0000

    versionCheck 1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,9 @@ games {'rdr3'}
 fx_version 'bodacious'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+description 'rsg-cinema'
+version '1.0.1'
+
 shared_scripts {
     'config.lua',
     '@ox_lib/init.lua'
